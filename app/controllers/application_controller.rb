523fadd2988405a38a3c3e13d80f96e0bda1793a
natty-mii <n.miya7@gmail.com>
2020-09-26 14:47:05 +0900

    add devise module and application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_pemitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_pemitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
